3ec12f7c4a0ee54d37ef155e7c5701662451bf56
TheMagicalAubster <aubrey.blairpattison@gmail.com>
2020-06-16 23:48:53 +0000

    tinyapp

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+
+//starting with the document.ready with the simple syntax
+$(() => {
+  const $textarea = jQuery('#tweet-text');
+  
+  $textarea.on('input', function() {
+    const $input = $(this);
+    const value = $input.val();
+    
+    const $counter = jQuery('output.counter', $input.parents('form'));
+
+    let charCount = 140 - value.length;
+    
+    if ((charCount) < 0) {
+      $counter.text(charCount).css('color', 'red');
+    }
+    $counter.text(charCount);
+
+  });
+
+});
